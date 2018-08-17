ea18173a2bcea1a80700e6d6859de24816b6e6f3
Padma Patil <padma@email.com>
2018-08-17 14:24:13 -0700

    This will now have if and else statements on the scripts file.

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  ("form#track-suggester").submit(function(event) {
+    event.preventDefault();
+
+    var iwebOrSoftware = ()
+
+
+    if(webOrSoftware === "yes")
+    {
+      if(backendOrFrontend ==="yes")
+        {
+          ("#ruby").show();
+        }
+      else
+        {
+          ("#php").show();
+        }
+    }
+
+    else
+    {
+      if(bigOrSmall === "yes")
+      {
+        if(mobileOrInternal === "yes")
+        {
+          ("#java").show();
+        }
+        else
+        {
+          ("#cSharp").show();
+        }
+      }
+      else
+      {
+        ("cSharp").show();
+      }
+    }
+
+
+  });
+
+});
